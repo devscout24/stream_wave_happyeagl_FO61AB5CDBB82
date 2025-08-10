abc5acbca6bae7bd76b4fba0d13b4404c9f17b55
Rashad-Stack <tex.eng.rashed@gmail.com>
2025-08-10 06:24:50 +0600

    refactor: simplify TermsModal component by removing unused state and imports

--- app/auth/sign-up/_components/TermsModal.tsx
+++ app/auth/sign-up/_components/TermsModal.tsx
@@ -16,9 +16,7 @@ import {
 
 import { Button } from "@/components/ui/button";
 import { DialogTrigger } from "@radix-ui/react-dialog";
-import { useState } from "react";
 import { useMediaQuery } from "react-responsive";
-import TermsServiceForm from "./TermsServiceForm";
 
 export default function TermsModal({
   onChange,
@@ -31,20 +29,15 @@ export default function TermsModal({
     query: "(min-width: 768px)",
   });
 
-  const [Open, setOpen] = useState(false);
-
   const handleOpenChange = (open: boolean) => {
     if (!open) {
-      setOpen(false);
     }
   };
 
   return isDesktopOrLaptop ? (
-    <Dialog open={Open} onOpenChange={handleOpenChange}>
-      <DialogTrigger className="cursor-pointer underline">
-        <Button type="button" variant="link">
-          Open Terms Modal
-        </Button>
+    <Dialog>
+      <DialogTrigger className="cursor-pointer underline" asChild>
+        <Button type="button">Open Terms Modal</Button>
       </DialogTrigger>
       <DialogContent className="text-muted-foreground bg-muted/5 dark:bg-muted w-full !max-w-xl rounded-md border bg-clip-padding p-10 backdrop-blur-md backdrop-filter dark:border-none">
         <DialogHeader className="space-y-3">
@@ -55,7 +48,8 @@ export default function TermsModal({
             Please read our terms before continuing to use the chatbot service.
           </DialogDescription>
         </DialogHeader>
-        <TermsServiceForm onChange={setOpen} value={Open} />
+        {/* <TermsServiceForm /> */}
+        Hello
       </DialogContent>
     </Dialog>
   ) : (
@@ -69,7 +63,8 @@ export default function TermsModal({
             Log in to access your personalized dashboard.
           </DrawerDescription>
         </DrawerHeader>
-        <TermsServiceForm onChange={setOpen} value={Open} />
+        {/* <TermsServiceForm  /> */}
+        hello
       </DrawerContent>
     </Drawer>
   );
